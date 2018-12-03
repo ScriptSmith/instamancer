44b49be9dcc6551281de3cf71cb29f020cfea4b7
Adam Smith <adamdevsmith@gmail.com>
2018-12-04 02:12:21 +1000

    batch input

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+import * as readline from 'readline';
+import * as fs from 'fs';
+
 import winston = require("winston");
 
 import {Hashtag, Location, User, ApiOptions} from "./src/api"
@@ -9,70 +12,119 @@ import {download, toCSV, toJSON} from "./src/download";
 /**
  * Build argument parser
  */
-require('yargs')
-    .command(
-        'hashtag [id]',
-        'Scrape a hashtag',
-        {},
-        async function f(args) {
-            await spawn(args);
-        }
-    )
-    .command(
-        'location [id]',
-        'Scrape a location',
-        {},
-        spawn
-    )
-    .command(
-        'user [id]',
-        'Scrape a user',
-        {},
-        spawn
-    )
-    .options({
-        'count': {
-            alias: 'c',
-            default: 0,
-            describe: 'Number of posts to download. 0 to download all'
-        },
-        'visible': {
-            default: false,
-            describe: 'Show browser on the screen'
-        },
-        'download': {
-            alias: 'd',
-            boolean: true,
-            default: false,
-            describe: "Save images and videos from posts"
-        },
-        'filename': {
-            alias: ['file', 'f', 'out'],
-            default: "[id]",
-            describe: "Name of the output file"
-        },
-        'filetype': {
-            alias: ['type', 't'],
-            default: 'json',
-            choices: ['csv', 'json', 'both'],
-            describe: "Type of output file "
-        },
-        'downdir': {
-            default: 'downloads',
-            describe: 'Directory to save thumbnails'
-        },
-        'logging': {
-            default: 'none',
-            choices: ['none', 'info', 'debug'],
-            describe: 'Level of logger'
-        },
-        'logfile': {
-            default: 'socialcreaper.log',
-            describe: 'Name of the log file'
-        }
-    })
-    .demandCommand()
-    .argv;
+function buildParser(args, callback) {
+    require('yargs')
+    (args)
+        .command(
+            'hashtag [id]',
+            'Scrape a hashtag',
+            {},
+            async (args) => {
+                await spawn(args);
+                callback();
+            }
+        )
+        .command(
+            'location [id]',
+            'Scrape a location',
+            {},
+            async (args) => {
+                await spawn(args);
+                callback();
+            }
+        )
+        .command(
+            'user [id]',
+            'Scrape a user',
+            {},
+            async (args) => {
+                await spawn(args);
+                callback();
+            }
+        )
+        .command(
+            'batch [batchfile]',
+            'Read a list of arguments from a file',
+            {},
+            () => {
+                // A list of functions which create new Promises that are
+                // resolved by buildParser when the spawn commands are
+                // finished
+                // See https://stackoverflow.com/a/45951080/7435520
+                let functions = [];
+
+                // Read the list of commands from file
+                readline.createInterface({
+                    input: fs.createReadStream(args[1]),
+                    crlfDelay: Infinity
+                })
+                    .on('line',
+                        // For each line, create a new function which
+                        // creates a new promise to be resolved by
+                        // buildParser
+                        (line) => {
+                            functions.push(
+                                () => new Promise(res =>
+                                    buildParser(line, res)
+                                )
+                            );
+                        })
+                    .on('close',
+                        // When all lines have been read, synchronously
+                        // execute the commands by waiting for their
+                        // promises to be resolved
+                        async () => {
+                            for (let i = 0; i < functions.length; i++) {
+                                await functions[i]();
+                            }
+                        });
+            }
+        )
+        .options({
+            'count': {
+                alias: 'c',
+                default: 0,
+                describe: 'Number of posts to download. 0 to download all'
+            },
+            'visible': {
+                default: false,
+                describe: 'Show browser on the screen'
+            },
+            'download': {
+                alias: 'd',
+                boolean: true,
+                default: false,
+                describe: "Save images and videos from posts"
+            },
+            'filename': {
+                alias: ['file', 'f', 'out'],
+                default: "[id]",
+                describe: "Name of the output file"
+            },
+            'filetype': {
+                alias: ['type', 't'],
+                default: 'json',
+                choices: ['csv', 'json', 'both'],
+                describe: "Type of output file "
+            },
+            'downdir': {
+                default: 'downloads',
+                describe: 'Directory to save thumbnails'
+            },
+            'logging': {
+                default: 'none',
+                choices: ['none', 'info', 'debug'],
+                describe: 'Level of logger'
+            },
+            'logfile': {
+                default: 'socialcreaper.log',
+                describe: 'Name of the log file'
+            }
+        })
+        .demandCommand()
+        .argv;
+}
+
 
 /**
  * Spawn an instance of the API
@@ -117,7 +169,8 @@ async function spawn(args) {
     for await (let post of obj.itr()) {
         posts.push(post);
         if (args['download']) {
-            download(post.node.thumbnail_src, post.node.shortcode, args['downdir'], () => {});
+            download(post.node.thumbnail_src, post.node.shortcode, args['downdir'], () => {
+            });
         }
     }
 
@@ -133,3 +186,5 @@ async function spawn(args) {
     }
 }
 
+buildParser(process.argv.slice(2), () => {
+});
