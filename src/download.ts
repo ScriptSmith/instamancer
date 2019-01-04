2324bf0daf0d68f8ca81c411ac800032770aa86b
ScriptSmith <ScriptSmith@users.noreply.github.com>
2019-01-04 13:50:51 +1000

    use default flattened fields for CSV output

--- src/download.ts
+++ src/download.ts
@@ -27,23 +27,11 @@ export async function download(url, name, directory, logger) {
     }
 }
 
-const fields = ["node.comments_disabled", "node.__typename", "node.id", "node.edge_media_to_caption.edges.0.node.text",
-    "node.shortcode", "node.edge_media_to_comment.count", "node.taken_at_timestamp", "node.dimensions.height",
-    "node.dimensions.width", "node.display_url", "node.edge_liked_by.count", "node.edge_media_preview_like.count",
-    "node.owner.id", "node.thumbnail_src", "node.thumbnail_resources.0.src", "node.thumbnail_resources.0.config_width",
-    "node.thumbnail_resources.0.config_height", "node.thumbnail_resources.1.src",
-    "node.thumbnail_resources.1.config_width", "node.thumbnail_resources.1.config_height",
-    "node.thumbnail_resources.2.src", "node.thumbnail_resources.2.config_width",
-    "node.thumbnail_resources.2.config_height", "node.thumbnail_resources.3.src",
-    "node.thumbnail_resources.3.config_width", "node.thumbnail_resources.3.config_height",
-    "node.thumbnail_resources.4.src", "node.thumbnail_resources.4.config_width",
-    "node.thumbnail_resources.4.config_height", "node.is_video", "node.video_view_count", "node.accessibility_caption"];
-
 /**
  * Save list of posts to a CSV file
  */
 export function toCSV(posts, filePath) {
-    const parser = new json2csv.Parser({fields});
+    const parser = new json2csv.Parser({flatten: true});
     const csv = parser.parse(posts);
     fs.writeFileSync(filePath, csv);
 }
