166422fd5c507fa354cab28053762b04b3b97e2b
ScriptSmith <ScriptSmith@users.noreply.github.com>
2019-08-29 16:29:24 +1000

    remove old-style functions

--- index.ts
+++ index.ts
@@ -1,30 +1,14 @@
 import {createApi} from "./src/api/api";
 
 export {
-  Hashtag,
-  Post,
-  User,
-  IOptions,
-  createApi,
-  IOptionsCommon,
-  IOptionsFullApi,
-  IOptionsRegular,
+    Hashtag,
+    Post,
+    User,
+    IOptions,
+    createApi,
+    IOptionsCommon,
+    IOptionsFullApi,
+    IOptionsRegular,
 } from "./src/api/api";
 export {TSearchResult, ISearchOptions} from "./src/api/search";
 export {TPost, TSinglePost, TFullApiPost} from "./src/api/types";
-
-export function hashtag(id, options) {
-  return createApi("hashtag", id, options).generator();
-}
-
-export function user(id, options) {
-  return createApi("user", id, options).generator();
-}
-
-export function post(ids, options) {
-  return createApi("post", ids, options).generator();
-}
-
-export function search(query, options) {
-  return createApi("search", query, options);
-}
