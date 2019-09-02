00a539e83783ce3cca7ce96063d87891744b86bd
ScriptSmith <ScriptSmith@users.noreply.github.com>
2019-09-02 17:59:10 +1000

    use provided override url when available

--- plugins/plugins/largeFirst.ts
+++ plugins/plugins/largeFirst.ts
@@ -14,14 +14,15 @@ export class LargeFirst implements IPlugin {
         overrides: Overrides,
         state: Instagram<DType>,
     ): void {
-        const url = urlParse(req.url());
-        const query = querystring.parse(url.query);
+        const url = overrides["url"] ? overrides["url"] : req.url();
+        const parsedUrl = urlParse(url);
+        const query = querystring.parse(parsedUrl.query);
         const variables = JSON.parse(query["variables"] as string);
 
         variables.first = 50;
 
         query.variables = JSON.stringify(variables);
-        url.search = "?" + querystring.stringify(query);
-        overrides["url"] = urlFormat(url);
+        parsedUrl.search = "?" + querystring.stringify(query);
+        overrides["url"] = urlFormat(parsedUrl);
     }
 }
